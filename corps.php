7bdbda34a4ab141cb3c463f476e859b025ecb503
clemisep <cde@isep.fr>
2014-10-28 11:04:36 +0100

    Début de restructuration pour rendre toutes les pages inhérentes à index.php

--- corps.php
+++ corps.php
@@ -4,5 +4,12 @@ function corps() {
     ?>
         <!-- Mettre le code HTML ici -->
     <?php
+        $page = $_GET['p'];
+        
+        if($page != null) {
+            include $page . ".php";
+        } else {
+            echo "Erreur : la page n'as pas été trouvée.";
+        }
 }
 ?>
