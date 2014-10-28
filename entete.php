7367e361ceb0e70e962d1565976e12342349fb7a
clemisep <cde@isep.fr>
2014-10-28 09:58:27 +0100

    Amélioration des boutons et insertion du champ pour rechercher

--- entete.php
+++ entete.php
@@ -2,11 +2,18 @@
 
 function entete() {
     ?>
-        <div id="entete">
-          <img src="logo.jpg"/>
-          <a href="index.php">Accueil</a>
-          <a href="annonces">Annonces</a>
-        </div>
+        <nav id="entete">
+          <img src="logo.jpg" id="logo"/>
+          <a href="index.php" class="item">Accueil</a>
+          <a href="annonces.php" class="item">Annonces</a>
+          <a href="forum.php" class="item">Forum</a>
+          <span class="item">
+            <a href="recherche.php">Rechercher : </a>
+            <form method="get" action="recherche.php">
+                <input type="text" name="recherche"/>
+            </form>
+          </span>
+        </nav>
     <?php
 }
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <body>
         <?php
             include "entete.php";
-
+            
             entete();
         ?>
     </body>
