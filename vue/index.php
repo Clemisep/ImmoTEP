4aa41bbccd7552a14499f48cb44f7e79f30dada6
ettilo <eliottdhommee@gmail.com>
2014-11-12 11:15:17 +0100

    caracteres speciaux

--- vue/index.php
+++ vue/index.php
@@ -2,6 +2,7 @@
     <head>
         <title>ImmoTEP, site d'échange de logements pour les vacances</title>
         <link rel="stylesheet" href="vue/style.css" />
+        <meta charset="utf-8">
     </head>
     
     <body>
