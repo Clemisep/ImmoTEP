4ecf241092caf83d0747961308b2cad4176f94cd
ettilo <eliottdhommee@gmail.com>
2014-11-25 10:03:55 +0100

    ajout connection inscription header

--- vue/entete_clement.php
+++ vue/entete_clement.php
@@ -1,15 +1,19 @@
 <div id="entete2">
     <img src="vue/logo.png" id="logo"/>
-    <div></div>
+    <div class="connect">
+        <div class="droite"></div>
+            <a href="?p=3" class="item">S'inscrire</a>
+            <a href="?p=3" class="item">Se connecter</a>
+    </div>
     <ul>
         <li><a href="index.php" class="item">Accueil</a></li>
         <li><a href="?p=2" class="item">Annonces</a></li>
         <li><a href="?p=1" class="item">Forum</a></li>
-        <li><a href="?p=3" class="item">Inscription</a></li>
-        <li><a href="recherche.php">Rechercher : </a></li>
+        <li><a href="?p=5">Rechercher : </a></li>
         <li><form method="get" action="recherche.php">
             <input type="text" name="recherche"/>
             <input type="submit" value="OK" />
             </form></li>
+        
     </ul>
 </div>
