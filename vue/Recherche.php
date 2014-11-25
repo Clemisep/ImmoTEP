8c43d20e33a9e48019c73bc6335ddd88db6084c1
ettilo <eliottdhommee@gmail.com>
2014-11-25 08:55:24 +0100

    changement menu

--- vue/Recherche.php
+++ vue/Recherche.php
@@ -1,57 +1,56 @@
-  <?php
-    /* indice : p=5 */
+<?php
+/* indice : p=5 */
 ?>
 
-            <div id="recherche">
-      <form method="post" action="recherche.php">
-      
-            Type de bien : <input type="text" name="pseudo"  />
-             
-             <br />
-             Localisation :<input type="text" name="localisation" />
-             
-             <br />
-             <fieldset>
-                  <legend>Nombre de pieces</legend> 
-                  Min : <input type="number" name="piecemin" />
-                  <br/>
-                  Max :<input type="number" name="piecemax" /> 
-             </fieldset>
-             
-             <br />
-             <fieldset>
-                  <legend>Surface</legend> 
-                  Min : <input type="number" name="surfacemin" />
-                  <br/>
-                  Max :<input type="number" name="surfacemax" /> 
-             </fieldset>
-             <br />
-             <fieldset>
-                  <legend>Periode</legend> 
-                  Debut : <input type="date" name="debut" /> 
-                  <br/>
-                  Fin :<input type="date" name="fin" /> 
-             </fieldset>
-             <br/>
-             <fieldset>
-                  <legend>Services</legend> 
-                 <input type="checkbox" name="services" value="fermer">Fermer la porte avant de partir
-                 <input type="checkbox" name="services" value="garderanimaux">Garder des animaux domestiques
-                 <input type="checkbox" name="services" value="aroserplante">Aroser les plantes
-                 <input type="checkbox" name="services" value="netoyer">Netoyer avant de partir
-             </fieldset>
-             <br />
-             <fieldset>
-                  <legend>Contraintes</legend>
-                 <input type="checkbox" name="contraintes" value="fumeur">Non fumeur
-                 <input type="checkbox" name="contraintes" value="pasdebruit">Pas de bruit apres 23h
-                 <input type="checkbox" name="contraintes" value="pasdenfant">Pas d'enfants
-                 <input type="checkbox" name="contraintes" value="pasanimaux">Pas d'animaux
-             </fieldset>
-             <br/>
-             Autres criteres  :<input type="text" name="critere"  />
-             <br/>
-             <input type="submit" value="Rechercher" />
-          </form>
-   </div>
-      
+<h2>Recherche</h2>
+<form method="post" action="recherche.php">
+
+    Type de bien : <input type="text" name="pseudo"  />
+
+    <br />
+    Localisation :<input type="text" name="localisation" />
+
+    <br />
+    <fieldset>
+        <legend>Nombre de pieces</legend> 
+        Min : <input type="number" name="piecemin" />
+        <br/>
+        Max :<input type="number" name="piecemax" /> 
+    </fieldset>
+
+    <br />
+    <fieldset>
+        <legend>Surface</legend> 
+        Min : <input type="number" name="surfacemin" />
+        <br/>
+        Max :<input type="number" name="surfacemax" /> 
+    </fieldset>
+    <br />
+    <fieldset>
+        <legend>Periode</legend> 
+        Debut : <input type="date" name="debut" /> 
+        <br/>
+        Fin :<input type="date" name="fin" /> 
+    </fieldset>
+    <br/>
+    <fieldset>
+        <legend>Services</legend> 
+        <input type="checkbox" name="services" value="fermer">Fermer la porte avant de partir
+        <input type="checkbox" name="services" value="garderanimaux">Garder des animaux domestiques
+        <input type="checkbox" name="services" value="aroserplante">Aroser les plantes
+        <input type="checkbox" name="services" value="netoyer">Netoyer avant de partir
+    </fieldset>
+    <br />
+    <fieldset>
+        <legend>Contraintes</legend>
+        <input type="checkbox" name="contraintes" value="fumeur">Non fumeur
+        <input type="checkbox" name="contraintes" value="pasdebruit">Pas de bruit apres 23h
+        <input type="checkbox" name="contraintes" value="pasdenfant">Pas d'enfants
+        <input type="checkbox" name="contraintes" value="pasanimaux">Pas d'animaux
+    </fieldset>
+    <br/>
+    Autres criteres  :<input type="text" name="critere"  />
+    <br/>
+    <input type="submit" value="Rechercher" />
+</form>
+
--- vue/entete_clement.php
+++ vue/entete_clement.php
@@ -1,14 +1,15 @@
-<nav id="entete2">
-  <img src="vue/logo.png" id="logo"/>
-  <a href="index.php" class="item">Accueil</a>
-  <a href="?p=2" class="item">Annonces</a>
-  <a href="?p=1" class="item">Forum</a>
-  <a href="?p=3" class="item">Inscription</a>
-  <span class="item">
-    <a href="recherche.php">Rechercher : </a>
-    <form method="get" action="recherche.php">
-        <input type="text" name="recherche"/>
-        <input type="submit" value="OK" />
-    </form>
-  </span>
-</nav>
+<div id="entete2">
+    <img src="vue/logo.png" id="logo"/>
+    <div></div>
+    <ul>
+        <li><a href="index.php" class="item">Accueil</a></li>
+        <li><a href="?p=2" class="item">Annonces</a></li>
+        <li><a href="?p=1" class="item">Forum</a></li>
+        <li><a href="?p=3" class="item">Inscription</a></li>
+        <li><a href="recherche.php">Rechercher : </a></li>
+        <li><form method="get" action="recherche.php">
+            <input type="text" name="recherche"/>
+            <input type="submit" value="OK" />
+            </form></li>
+    </ul>
+</div>
