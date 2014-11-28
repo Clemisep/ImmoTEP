b5ca3e39d6725c6c26859af930a2c77ea290cfef
zakiakazi <zkazi@isep.fr>
2014-11-28 14:34:30 +0100

    test Mme Kazi

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+        
         <title>ImmoTEP, site d'échange de logements pour les vacances</title>
         <link rel="stylesheet" href="vue/style.css" />
         <link rel="stylesheet" href="vue/banniere.css">
