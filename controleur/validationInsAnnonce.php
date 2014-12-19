c4d429863eb6f72b2fd6f684f140fe0b0d4e1a18
Clement <Clement@Clemordi>
2014-12-19 09:38:17 +0100

    Dépôt d'annonce avec affichage des erreurs et remplissage automatique. Corrections, modifications et remise des modifications que j'avais faites et qu'on m'a écrasées.

--- controleur/validationInsAnnonce.php
+++ controleur/validationInsAnnonce.php
@@ -10,25 +10,96 @@ $erreursInsAnnonce = [];
 
 $idMembre = recIdMembre($_SESSION["login"]);
 
+if($idMembre == 0) {
+   $erreursInsAnnonce["connexion"] = "Vous devez vous connecter pour déposer une annonce";
+}
+
+if(emptyPost('titre')) {
+    $erreursInsAnnonce['titre'] = "Veuillez donner un titre à votre logement";
+}
+
+if(emptyPost('rue')) {
+    $erreursInsAnnonce['rue'] = "Veuillez indiquer la rue de votre logement";
+}
+
+if(emptyPost('numero')) {
+    $erreursInsAnnonce['numero'] = "Veuillez indiquer le numéro du logement";
+}
+
+if(emptyPost('ville')) {
+   $erreursInsAnnonce['ville'] = "Veuillez indiquer la ville";
+}
+
+if(emptyPost('codePostal')) {
+    $erreursInsAnnonce['codePostal'] = "Veuillez indiquer le code postal";
+}
+
+if(emptyPost('typeMaison')) {
+    $erreursInsAnnonce['typeMaison'] = "Veuillez indiquer le type de logement";
+}
+
+if(emptyPost('nombreDeChambres')) {
+    $erreursInsAnnonce['nombreDeChambres'] = "Veuillez indiquer le nombre de chambres";
+}
+
+if(emptyPost('nombreDeLits')) {
+    $erreursInsAnnonce['nombreDeLits'] = "Veuillez indiquer le nombre de lits";
+}
+
+if(emptyPost('nombreDeSallesDeBain')) {
+    $erreursInsAnnonce['nombreDeSallesDeBain'] = "Veuillez indiquer le nombre de salles de bain";
+}
+
+if(emptyPost('superficie')) {
+    $erreursInsAnnonce['superficie'] = "Veuillez indiquer la superficie";
+}
+
+if(emptyPost('description')) {
+    $erreursInsAnnonce['description'] = "Veuillez décrire le logement";
+}
+
+$titre = recPostOuVide('titre');
+$rue = recPostOuVide('rue');
+$numero = recPostOuVide('numero');
+$ville = recPostOuVide('ville');
+$codePostal = recPostOuVide('codePostal');
+$pays = "France";
+$typeDeLogement = recPostOuVide('typeMaison');
+$nombreDeChambres = recPostOuVide('nombreDeChambres');
+$nombreDeLits = recPostOuVide('nombreDeLits');
+$nombreDeSallesDeBain = recPostOuVide('nombreDeSallesDeBain');
+$superficie = recPostOuVide('superficie');
+
+//echo "equipements = ".gettype($equipements = recPostOuTabVide('avantages'));
+$equipements = recPostOuTabVide('avantages');
+$services = recPostOuTabVide('services');
+$contraintes = recPostOuTabVide('contraintes');
+
+$description = recPost('description');
+
 if(empty($erreursInsAnnonce)) {
-    $titre = recPost('nom');
-    $rue = recPost('rue');
-    $numero = recPost('numero');
-    $ville = recPost('ville');
-    $codePostal = recPost('codepostal');
-    $pays = "France";
-    $typeDeLogement = recPost('typeMaison');
-    $nombreDeChambres = recPost('nbChambre');
-    $nombreDeLits = recPost('nbCouchage');
-    $nombreDeSallesDeBain = recPost('nbSalleDeBain');
-    $superficie = recPost('superficie');
-    
-    //echo "equipements = ".gettype($equipements = recPostOuTabVide('avantages'));
-    $equipements = recPostOuTabVide('avantages');
-    $services = recPostOuTabVide('services');
-    $contraintes = recPostOuTabVide('contraintes');
-    
-    $description = recPost('description');
-    
     ajouterAnnonce($titre, $rue, $numero, $ville, $codePostal, $pays, $typeDeLogement, $nombreDeChambres, $nombreDeLits, $nombreDeSallesDeBain, $superficie, $equipements, $services, $contraintes, $description, $idMembre);
+    
+    include $pages[0];
+    
+} else {
+    $remplisAnnonce = array(
+        'titre' => $titre,
+        'rue' => $rue,
+        'numero' => $numero,
+        'ville' => $ville,
+        'codePostal' => $codePostal,
+        'pays' => $pays,
+        'typeDeLogement' => $typeDeLogement,
+        'nombreDeChambres' => $nombreDeChambres,
+        'nbDeLits' => $nombreDeLits,
+        'nombreDeSallesDeBain' => $nombreDeSallesDeBain,
+        'superficie' => $superficie,
+        'equipements' => $equipements,
+        'services' => $services,
+        'contraintes' => $contraintes,
+        'description' => $description
+    );
+    
+    include $pages[6];
 }
--- vue/annonces/insmaison.php
+++ vue/annonces/insmaison.php
@@ -1,45 +1,62 @@
+<?php
+    $champs = array('titre', 'rue', 'numero', 'ville', 'codePostal', 'typeMaison', 'nombreDeChambres', 'nombreDeLits', 'nombreDeSallesDeBain', 'superficie', 'description');
+    
+    if(empty($remplisAnnonce)) {
+        $remplisAnnonce = [];
+    }
+    
+    if(empty($erreursInsAnnonce)) {
+        $erreursInsAnnonce = [];
+    }
+    
+    foreach($champs as $clef => $valeur) {
+        if(!array_key_exists($valeur, $remplisAnnonce)) {
+            $remplisAnnonce[$valeur] = "";
+        }
+        
+        if(!array_key_exists($valeur, $erreursInsAnnonce)) {
+            $erreursInsAnnonce[$valeur] = "";
+        }
+    }
+?>
+
 <center><h2><br>Ajouter une maison </h2></center>
 
-<form id=maison>
+<form id="maison" name="FormulaireAnnonce" action="?p=11" method="post" onSubmit="return verification();">
     <fieldset>
-        <legend><h4> Adresse</h4></legend>
+        <legend><h4>Adresse</h4></legend>
         <table border="0" cellpadding="5" cellspacing="15">
             <tr>
-                <td><label for="nom">Nommer son logement : </label></td>
-                <td><input type="text" id="nom" name="nom" maxlength="60" value="<?php if (isset($_POST['valider'])) {
-                        echo $_POST['nom'];
-                    } ?>"></td>
+                <td><label for="titre">Nommer son logement : </label></td>
+                <td><input type="text" id="titre" name="titre" maxlength="60" value="<?php echo $remplisAnnonce['titre']; ?>"></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["titre"]; ?></span></td>
             </tr>
 
             <tr>
-                <td><label for="adresse">Rue : </label></td>
-                <td><input type="text" id="adresse" name="adresse" maxlength="60"
-                           value="<?php if (isset($_POST['valider'])) {
-                               echo $_POST['adresse'];
-                           } ?>"></td>
+                <td><label for="rue">Rue : </label></td>
+                <td><input type="text" id="rue" name="rue" maxlength="60"
+                           value="<?php echo $remplisAnnonce['rue']; ?>"></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["rue"]; ?></span></td>
             </tr>
             <tr>
                 <td><label for="numero">Numéro : </label></td>
                 <td><input type="text" id="numero" name="numero" maxlength="60"
-                           value="<?php if (isset($_POST['valider'])) {
-                               echo $_POST['numero'];
-                           } ?>"></td>
+                           value="<?php echo $remplisAnnonce['numero']; ?>"></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["numero"]; ?></span></td>
             </tr>
 
             <tr>
                 <td><label for="ville">Ville : </label></td>
                 <td><input type="text" id="ville" name="ville" maxlength="60"
-                           value="<?php if (isset($_POST['valider'])) {
-                               echo $_POST['ville'];
-                           } ?>"></td>
+                           value="<?php echo $remplisAnnonce['ville']; ?>"></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["ville"]; ?></span></td>
             </tr>
 
             <tr>
-                <td><label for="codepostal">Code postal : </label></td>
-                <td><input type="text" id="codepostal" name="codepostal" maxlength="60"
-                           value="<?php if (isset($_POST['valider'])) {
-                               echo $_POST['codepostal'];
-                           } ?>"></td>
+                <td><label for="codePostal">Code postal : </label></td>
+                <td><input type="text" id="codePostal" name="codePostal" maxlength="60"
+                           value="<?php echo $remplisAnnonce['codePostal']; ?>"></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["codePostal"]; ?></span></td>
             </tr>
             <br/>
         </table>
@@ -48,39 +65,47 @@
 
     <fieldset>
         <table border="0" cellpadding="5" cellspacing="15">
-            <legend><h4>Logement :</h4></legend>
+            <legend><h4>Logement</h4></legend>
             <br>
 
             <tr>
                 <td><label for="typeMaison"> Type de logement : </label></td>
                 <td>
                     <select name="typeMaison" size="1">
-                        <option>Maison
-                        <option>Apartement
-                        <option>Chateau
-                        <option>Villa
-                        <option>Bungallow
-                        <option>Chalet
-                        <option>Autre
+                        <option>Maison</option>
+                        <option>Apartement</option>
+                        <option>Chateau</option>
+                        <option>Villa</option>
+                        <option>Bungallow</option>
+                        <option>Chalet</option>
+                        <option>Autre</option>
                     </select>
             </tr>
             </td>
 
             <tr>
-                <td><label for="nbChambre">Nombre de chambres : </label></td>
-                <td><input type="number" name="nbChambre"/></td>
+                <td><label for="nombreDeChambres">Nombre de chambres : </label></td>
+                <td><input type="number" name="nombreDeChambres"
+                           value="<?php echo $remplisAnnonce['nombreDeChambres']; ?>" /></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["nombreDeChambres"]; ?></span></td>
             </tr>
             <tr>
-                <td><label for="nbCouchage">Nombre de couchages : </label></td>
-                <td><input type="number" name="nbCouchage"/></td>
+                <td><label for="nombreDeLits">Nombre de couchages : </label></td>
+                <td><input type="number" name="nombreDeLits"
+                           value="<?php echo $remplisAnnonce['nombreDeLits']; ?>"/></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["nombreDeLits"]; ?></span></td>
             </tr>
             <tr>
-                <td><label for="nbSalleDeBain">Nombre de salle de bain : </label></td>
-                <td><input type="number" name="nbSalleDeBain"/></td>
+                <td><label for="nombreDeSallesDeBain">Nombre de salle de bain : </label></td>
+                <td><input type="number" name="nombreDeSallesDeBain"
+                           value="<?php echo $remplisAnnonce['nombreDeSallesDeBain']; ?>" /></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["nombreDeSallesDeBain"]; ?></span></td>
             </tr>
             <tr>
                 <td><label for="superficie">Superficie : </label></td>
-                <td><input type="number" name="superficie"/></td>
+                <td><input type="number" name="superficie"
+                           value="<?php echo $remplisAnnonce['superficie']; ?>"/></td>
+                <td><span class="formulaireErreur"><?php echo $erreursInsAnnonce["superficie"]; ?></span></td>
             </tr>
         </table>
     </fieldset>
@@ -89,46 +114,46 @@
 
     <fieldset>
         
-           <legend><h4>Equipement(s) :</h4></legend>
+           <legend><h4>Equipement(s)</h4></legend>
         <br>
     <table class="mathilde">
 		<tr>
         <td class="equipement">
           <p>Pour la détente:</p>
-            <p><input type="checkbox" name="avantages" value="BalconTerrasse"> Balcon/Terrasse </p>
-            <p><input type="checkbox" name="avantages" value="Transat"> Transat</p>
-            <p><input type="checkbox" name="avantages" value="Tabledejardin"> Table de jardin</p>
-            <p><input type="checkbox" name="avantages" value="Piscine"> Piscine</p>
-            <p><input type="checkbox" name="avantages" value="Piano"> Piano</p>
-            <p><input type="checkbox" name="avantages" value="Jacuzzi"> Jacuzzi</p>
-            <p><input type="checkbox" name="avantages" value="Télévision"> Télévision</p>
+            <p><input type="checkbox" name="equipements[]" value="BalconTerrasse"> Balcon/Terrasse </p>
+            <p><input type="checkbox" name="equipements[]" value="Transat"> Transat</p>
+            <p><input type="checkbox" name="equipements[]" value="Tabledejardin"> Table de jardin</p>
+            <p><input type="checkbox" name="equipements[]" value="Piscine"> Piscine</p>
+            <p><input type="checkbox" name="equipements[]" value="Piano"> Piano</p>
+            <p><input type="checkbox" name="equipements[]" value="Jacuzzi"> Jacuzzi</p>
+            <p><input type="checkbox" name="equipements[]" value="Télévision"> Télévision</p>
         </td>
        
         <td class="equipement">
           <p>Pour la propreté:</p>  
-            <p><input type="checkbox" name="avantages" value="Lavevaisselle"> Lave vaisselle</p>
-            <p><input type="checkbox" name="avantages" value="Machineàlaver"> Machine à laver</p>
-            <p><input type="checkbox" name="avantages" value="Sèchelinge"> Sèche-linge</p>
-            <p><input type="checkbox" name="avantages" value="Douche"> Douche</p>
-           <p><input type="checkbox" name="avantages" value="Baignoire"> Baignoire</p>   
+            <p><input type="checkbox" name="equipements[]" value="Lavevaisselle"> Lave vaisselle</p>
+            <p><input type="checkbox" name="equipements[]" value="Machineàlaver"> Machine à laver</p>
+            <p><input type="checkbox" name="equipements[]" value="Sèchelinge"> Sèche-linge</p>
+            <p><input type="checkbox" name="equipements[]" value="Douche"> Douche</p>
+           <p><input type="checkbox" name="equipements[]" value="Baignoire"> Baignoire</p>   
         </td>
        
         <td class="equipement"> 
             <p>Pour l'accessibilitié:</p>
-            <p><input type="checkbox" name="avantages" value="Ascenceur"> Ascenceur</p>
-            <p><input type="checkbox" name="avantages" value="Garage"> Garage</p>
-            <p><input type="checkbox" name="avantages" value="Cave"> Cave</p>
-            <p><input type="checkbox" name="contraintes" value="Acceshandicapes"> Accès handicapés</p>
-            <p><input type="checkbox" name="avantages" value="Grenier"> Grenier</p>
+            <p><input type="checkbox" name="equipements[]" value="Ascenceur"> Ascenceur</p>
+            <p><input type="checkbox" name="equipements[]" value="Garage"> Garage</p>
+            <p><input type="checkbox" name="equipements[]" value="Cave"> Cave</p>
+            <p><input type="checkbox" name="equipements[]" value="Acceshandicapes"> Accès handicapés</p>
+            <p><input type="checkbox" name="equipements[]" value="Grenier"> Grenier</p>
         </td>
         
         <td class="equipement">
             <p>Pour le confort: </p>
-            <p><input type="checkbox" name="avantages" value="Microonde"> Micro-ondes</p>
-            <p><input type="checkbox" name="avantages" value="Four"> Four</p>
-            <p><input type="checkbox" name="avantages" value="Climatisation"> Climatisation</p>
-            <p><input type="checkbox" name="avantages" value="Cheminée"> Cheminée</p>
-            <p><input type="checkbox" name="avantages" value="Wifi"> Wifi</p>
+            <p><input type="checkbox" name="equipements[]" value="Microonde"> Micro-ondes</p>
+            <p><input type="checkbox" name="equipements[]" value="Four"> Four</p>
+            <p><input type="checkbox" name="equipements[]" value="Climatisation"> Climatisation</p>
+            <p><input type="checkbox" name="equipements[]" value="Cheminée"> Cheminée</p>
+            <p><input type="checkbox" name="equipements[]" value="Wifi"> Wifi</p>
         </td>
 		</tr>
       <tr>
@@ -142,13 +167,13 @@
     </fieldset>
     <br/>
     <fieldset>
-        <legend><h4>Services : </h4></legend>
+        <legend><h4>Services</h4></legend>
         <table>
         <tr>
-		  <td><p><input type="checkbox" name="services" value="fermer">Fermer la porte avant de partir</p>
-            <p><input type="checkbox" name="services" value="garderanimaux">Garder des animaux domestiques</p></td>
-          <td><p><input type="checkbox" name="services" value="aroserplante">Arroser les plantes</p>
-            <p><input type="checkbox" name="services" value="netoyer">Nettoyer avant de partir</p></td>
+		  <td><p><input type="checkbox" name="services[]" value="fermer">Fermer la porte avant de partir</p>
+            <p><input type="checkbox" name="services[]" value="garderanimaux">Garder des animaux domestiques</p></td>
+          <td><p><input type="checkbox" name="services[]" value="aroserplante">Arroser les plantes</p>
+            <p><input type="checkbox" name="services[]" value="netoyer">Nettoyer avant de partir</p></td>
 		</tr>
 		 <tr>
             <td>Autres services demandés :</td>
@@ -160,11 +185,11 @@
     <fieldset>
         <legend><h4>Contraintes</h4></legend>
 		<table>
-        <tr><td><input type="checkbox" name="contraintes" value="fumeur">Non fumeur</td>
-   	   <td><input type="checkbox" name="contraintes" value="pasdebruit">Pas de bruit après 23h</td></tr>
+        <tr><td><input type="checkbox" name="contraintes[]" value="fumeur">Non fumeur</td>
+   	   <td><input type="checkbox" name="contraintes[]" value="pasdebruit">Pas de bruit après 23h</td></tr>
 	   <tr></tr>
-        <tr><td><input type="checkbox" name="contraintes" value="pasdenfant">Pas d'enfants</td>
-        <td><input type="checkbox" name="contraintes" value="pasanimaux">Pas d'animaux</td></tr>
+        <tr><td><input type="checkbox" name="contraintes[]" value="pasdenfant">Pas d'enfants</td>
+        <td><input type="checkbox" name="contraintes[]" value="pasanimaux">Pas d'animaux</td></tr>
 		 <tr>
             <td><span>Autres contraintes imposées :</td>
             <td><textarea name="contenu" rows="3" cols="30">Votre texte...</textarea></span></td>
@@ -174,16 +199,24 @@
     <br/>
 
     <fieldset>
-        <legend><h4>Description : </h4></legend>
+        <legend><h4>Description</h4></legend>
         <br/>
-        <textarea name="texte" rows="3" cols="30">Votre texte...</textarea>
+        <textarea name="description" rows="3" cols="30"><?php echo $remplisAnnonce['description']; ?></textarea>
+        <span class="formulaireErreur"><?php echo $erreursInsAnnonce["description"]; ?></span>
     </fieldset>
     <br/>
     <br/>
 
-    Ajouter une photo : <br>
-    <input type="file" name="Ajouter une photo" maxlength="200000" accept="text/*"><br>
-
+    <fieldset>
+        <legend><h4>Photos</h4></legend>
+        <p>Vous devez inclure au moins 3 photos de votre logement.</p>
+        <input type="file" name="photosAnnonce[]" maxlength="200000" accept="text/*"/><br>
+        <input type="file" name="photosAnnonce[]" maxlength="200000" accept="text/*"/><br>
+        <input type="file" name="photosAnnonce[]" maxlength="200000" accept="text/*"/><br>
+        <input type="button" value="Ajouter une photo"/>
+    </fieldset>
+    <br/>
+    <br/>
 
     <tr>
         <td align="center" colspan="2"><input type="submit" name="valider" class="valider" value="Ajouter maison"
