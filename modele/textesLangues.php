d55015e6f453b230c8b44ba1f85086e6f9821bab
ymeziani <ymeziani@pc-l312-7.isep.fr>
2015-01-05 15:31:50 +0100

    test anglais

---
+++ modele/textesLangues.php
@@ -0,0 +1,15 @@
+<?php
+
+$txtErreur1 = array("Erreur : la page n'a pas été trouvée." , "Error: page not found.");
+$txtdepoannonce = array("Vous devez vous connecter pour déposer une annonce" , );
+$txttitrelogement = array("Veuillez donner un titre à votre logement" , );
+$txtruelogement = array("Veuillez indiquer la rue de votre logement" , );
+$txtnumerologement = array("Veuillez indiquer le numéro du logement" , );
+$txtindiquerville = array("Veuillez indiquer la ville" , );
+$txtindiquercodepost = array("Veuillez indiquer le code postal" , );
+$txttypelogement = array("Veuillez indiquer le type de logement" , );
+$txtnbrchambres = array("Veuillez indiquer le nombre de chambres" , );
+$txtnbrlits = array("Veuillez indiquer le nombre de lits" , );
+$txtnbrsallebain = array("Veuillez indiquer le nombre de salles de bain" , );
+$txtindiquersuperficie = array("Veuillez indiquer la superficie" , );
+$txtdecrirelogement = array("Veuillez décrire le logement" , );
