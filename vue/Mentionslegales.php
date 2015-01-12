ab1af2dbc321c03b019322abc6d7e1082d156ad4
meziani <yanis.meziani@isep.fr>
2015-01-12 14:42:14 +0100

    Create Mentionslegales.php

---
+++ vue/Mentionslegales.php
@@ -0,0 +1,93 @@
+<?php if($numeroLangue == 0) { ?>
+<h2> Mentions Légales</h2><br/>
+<fieldset>
+<legend><h4>Protection des données personnelles :</h4></legend>
+
+<p>Les données transmises par nos annonceurs sont confidentielles et, en aucun cas ne font l'objet de transmission à des tiers, sauf en cas de réquisition judiciaire ou de demande par une autorité habilitée.</p>
+
+<p>Conformément à la loi informatique et liberté du 6 janvier 1978, vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données vous concernant. Pour exercer ce droit, vous pouvez vous adresser à "CLIENTE".</p>
+
+<p>Le responsable de la collecte et du traitement des données personnelles est : "CLIENTE"</p>
+
+<p>Lorsque vous passez votre annonce, il est nécessaire que vous communiquiez à ImmoTEP le type de bien que vous proposez, le lieu où il se situe, le texte de l'annonce, les services que vous demandez, le numéro de téléphone et/ou l'adresse e-mail où vous souhaitez que les personnes intéressées vous contactent. ImmoTEP collecte aussi vos coordonnées personnelles, nom, prénom adresse, numéro de téléphone, e-mail. Ces données sont strictement confidentielles et ne sont pas transmises à des tiers, sauf réquisition judiciaire ou demande d'une autorité habilitée. Ces données sont obligatoires pour le traitement de votre annonce et dans le cas où vous refusez de les communiquer, nous ne sommes pas en mesure de publier votre annonce.</p>
+
+<p>Lorsque vous vous connectez aux services de ImmoTEP par Internet, nous conservons la trace de ces connexions électroniques.</p>
+</fieldset>
+<br>
+
+<fieldset>
+<legend><h4>Finalité des traitements :</h4></legend>
+
+<p>La conservation et le traitement des données ci-dessus énumérées répondent aux finalités suivantes :</p>
+
+    <ul>
+        <li>Gestion de la relation commerciale ;</li>
+        <li>Gestion de la comptabilité client ;</li>
+        <li>Constitution et gestion de fichiers de clients et de prospects ;</li>
+        <li>Analyses statistiques à des fins commerciales et études relatives au marché immobilier ;</li>
+        <li>Envoi ultérieur de lettres d'information par mail, de propositions commerciales par mail, courrier ou SMS.</li>
+        <li>Si vous ne désirez pas recevoir les lettres d'information ou les propositions commerciales de ImmoTEP, contactez-nous au "NUMERO DE TELEPHONE" .</li>
+        <li>Durée de conservation des données à caractère personnel</li>
+    </ul>
+
+<p>Les données concernant votre annonce sont conservées dans une base de gestion durant un an après notre dernière relation commerciale, et dans une base historique compressée durant 5 ans, afin de satisfaire aux obligations légales concernant les administrations et les autorités judiciaires.</p>
+</fieldset>
+<br>
+
+<fieldset>
+<legend><h4>Sécurité des données :</h4></legend>
+
+<p>ImmoTEP prend toutes les mesures nécessaires pour garantir la sécurité des données stockées, notamment pour empêcher que des tiers non autorisés puissent y accéder.</p>
+</fieldset>
+
+<?php } else { ?>
+
+<h2> Mentions Légales</h2><br/>
+<fieldset>
+<legend><h4>Protection des données personnelles :</h4></legend>
+
+<p>Les données transmises par nos annonceurs sont confidentielles et, en aucun cas ne font l'objet de transmission à des tiers, sauf en cas de réquisition judiciaire ou de demande par une autorité habilitée.</p>
+
+<p>Conformément à la loi informatique et liberté du 6 janvier 1978, vous disposez d'un droit d'accès, de modification, de rectification et de suppression des données vous concernant. Pour exercer ce droit, vous pouvez vous adresser à "CLIENTE".</p>
+
+<p>Le responsable de la collecte et du traitement des données personnelles est : "CLIENTE"</p>
+
+<p>Lorsque vous passez votre annonce, il est nécessaire que vous communiquiez à ImmoTEP le type de bien que vous proposez, le lieu où il se situe, le texte de l'annonce, les services que vous demandez, le numéro de téléphone et/ou l'adresse e-mail où vous souhaitez que les personnes intéressées vous contactent. ImmoTEP collecte aussi vos coordonnées personnelles, nom, prénom adresse, numéro de téléphone, e-mail. Ces données sont strictement confidentielles et ne sont pas transmises à des tiers, sauf réquisition judiciaire ou demande d'une autorité habilitée. Ces données sont obligatoires pour le traitement de votre annonce et dans le cas où vous refusez de les communiquer, nous ne sommes pas en mesure de publier votre annonce.</p>
+
+<p>Lorsque vous vous connectez aux services de ImmoTEP par Internet, nous conservons la trace de ces connexions électroniques.</p>
+</fieldset>
+<br>
+
+<fieldset>
+<legend><h4>Finalité des traitements :</h4></legend>
+
+<p>La conservation et le traitement des données ci-dessus énumérées répondent aux finalités suivantes :</p>
+
+    <ul>
+        <li>Gestion de la relation commerciale ;</li>
+        <li>Gestion de la comptabilité client ;</li>
+        <li>Constitution et gestion de fichiers de clients et de prospects ;</li>
+        <li>Analyses statistiques à des fins commerciales et études relatives au marché immobilier ;</li>
+        <li>Envoi ultérieur de lettres d'information par mail, de propositions commerciales par mail, courrier ou SMS.</li>
+        <li>Si vous ne désirez pas recevoir les lettres d'information ou les propositions commerciales de ImmoTEP, contactez-nous au "NUMERO DE TELEPHONE" .</li>
+        <li>Durée de conservation des données à caractère personnel</li>
+    </ul>
+
+<p>Les données concernant votre annonce sont conservées dans une base de gestion durant un an après notre dernière relation commerciale, et dans une base historique compressée durant 5 ans, afin de satisfaire aux obligations légales concernant les administrations et les autorités judiciaires.</p>
+</fieldset>
+<br>
+
+<fieldset>
+<legend><h4>Sécurité des données :</h4></legend>
+
+<p>ImmoTEP prend toutes les mesures nécessaires pour garantir la sécurité des données stockées, notamment pour empêcher que des tiers non autorisés puissent y accéder.</p>
+</fieldset>
+
+<?php } ?>
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
