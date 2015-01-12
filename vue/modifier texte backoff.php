2432c4010891c1b68c7cfffa473abc5e096e31f7
mathildeabdelaziz <math.abdelaziz@gmail.com>
2015-01-12 14:33:06 +0100

    Ajout.

---
+++ vue/modifier texte backoff.php
@@ -0,0 +1,32 @@
+<?php 
+/* Indice : p=16 */ 
+Modifier le texte de la page d'accueil.
+?>
+<form action="scripts/traiter_form.php" method="post">
+	<fieldset>
+            <legend><h4><b>Titre:</b></h4></legend>
+                <p> </br>Votre projet immobilier
+                </br>Nouveau titre :<TEXTAREA name="nom" cols="25" rows="3">Votre projet immobilier</TEXTAREA>
+            </p>
+                <INPUT type="submit" name="nom" value="Envoyer">
+        </fieldset>
+</form> 
+
+<form action="scripts/traiter_form.php" method="post">
+	<fieldset>
+            <legend><h4><b>Texte:</b></h4></legend>
+                <p>
+                </br>Grâce à ImmoTEP vous pouvez trouver le logement idéal pour vous, gratuitement, à condition de réaliser les services demandés par le propriétaire et de respecter les contrtaintes indiquées. ImmoTEP vous propose :
+                </br></br>Un service <b>Gratuit</b>,
+                </br>Des annonces <b>Détaillées</b>,
+                </br>Un <b>Forum</b> facile à utiliser
+
+</br></br></br>Nouveau texte: <form><TEXTAREA name="nom" cols="50" rows="10">Grâce à ImmoTEP vous pouvez trouver le logement idéal pour vous, gratuitement, à condition de réaliser les services demandés par le propriétaire et de respecter les contrtaintes indiquées. ImmoTEP vous propose :
+
+Un service Gratuit,
+Des annonces Détaillées,
+Un Forum facile à utiliser</TEXTAREA></form>
+<INPUT type="submit" name="nom" value="Envoyer">
+</p>
+</fieldset>
+</form> 
