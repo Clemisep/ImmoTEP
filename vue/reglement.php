ff24b86ecc1a9b275d931a3a10a348fbe8524b41
mathildeabdelaziz <math.abdelaziz@gmail.com>
2015-01-13 09:38:29 +0100

    modif reglement 2

--- vue/reglement.php
+++ vue/reglement.php
@@ -2,7 +2,7 @@
 <center><h2>REGLEMENT</h2></center>
 <br>
 <br>
-<p>En publiant votre annonce dans le groupe De Particulier à Particulier, vous acceptez les conditions suivantes :
+<p>En publiant votre annonce dans le groupe ImmoTEP.com, vous acceptez les conditions suivantes :
 
 <br>
 
@@ -14,13 +14,12 @@
 <fieldset>
 	<legend><h4>L’annonce</h4></legend>
 	<p>Vous garantissez que les renseignements fournis sont exacts et ne sont entachés d'aucune erreur ou omission de caractère trompeur ou malhonnête. Notre responsabilité, en tant que Société éditrice ne pourra être engagée, à quel que titre que ce soit, si les renseignements portés dans l'annonce ou si la qualité de l'annonceur se révèlent inexactes.</p>
-	<p>Nous ne diffusons qu'un seul bien par annonce. Le texte, les coordonnées téléphoniques et le prix figurant dans les annonces peuvent être modifiés gratuitement en cours de parution. Toutefois le bien décrit doit rester celui d'origine sans possibilité d'y substituer un autre bien.</p>
+	<p>Nous ne diffusons qu'un seul bien par annonce. Le texte et les coordonnées téléphoniques figurant dans les annonces peuvent être modifiés gratuitement en cours de parution. Toutefois le bien décrit doit rester celui d'origine sans possibilité d'y substituer un autre bien.</p>
 </fieldset>
 <br>
 <fieldset>
 	<legend><h4>Forfaits</h4></legend>
-	<p>Votre annonce sera diffusée sur notre site pap.fr selon les tarifs et durée que vous avez choisis, le prix de ces forfaits étant indiqués sur toutes nos offres et supports.</p>
-	<p>Nos tarifs sont forfaitaires et s'entendent pour des parutions consécutives. Dès qu'une annonce est publiée, son règlement nous reste acquis, même si celle-ci est arrêtée avant la fin de sa parution.</p>
+        <p>Nos services sont complètement gratuit. Si quiquoncque vous demande un service payant veuillez nous contacter. </p>
 </fieldset>
 <br>
 <fieldset>
@@ -44,7 +43,6 @@
 <p>Seules les adresses de sites internet personnels pourront être publiées dans l'annonce après vérification par nos services.
 </p>
 </fieldset>
-
 
 
 
