8e38b5d5508cdca8627940dcaf85475405b068d1
yanis meziani <ymeziani@pc-l312-7.isep.fr>
2015-01-16 09:16:51 +0100

    fin trad

--- index.php
+++ index.php
@@ -17,9 +17,19 @@
             $_SESSION["lang"]=0;
         }
         
+        if(tstGet("lang") !=false) {
+            if(recGet("lang")=="en"){
+                $_SESSION["lang"] = 1;
+            }
+            if(recGet("lang")=="fr"){
+                $_SESSION["lang"] = 0;
+            }
+        }
+        
         
         
         $numeroLangue = $_SESSION["lang"];
+        
         
         
         
