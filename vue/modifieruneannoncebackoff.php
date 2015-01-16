35aaee0d93bf1b3103046fdfb3e8fb9f495ef766
mabdelaziz <mabdelaziz@pc-l305-18.isep.fr>
2015-01-16 15:04:04 +0100

    arrangement bo3

--- vue/modifieruneannoncebackoff.php
+++ vue/modifieruneannoncebackoff.php
@@ -3,5 +3,6 @@
 ?>
 <form action="scripts/traiter_form.php" method="post">
 <fieldset>
-<legend style="font-size:100px;pading-left:60px;">yttytytytytytyty</legend>
+<legend> Modifier un annonce:</legend>
+
 </fieldset>
