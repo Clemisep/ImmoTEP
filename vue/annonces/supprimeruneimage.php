a5edbe4414e8fb060a264c7f397caf98ef7f7d71
Clemisep <ubuwusenno-9596@yopmail.com>
2015-01-20 15:08:14 +0100

    Réorganisation.

--- vue/annonces/supprimeruneimage.php
+++ vue/annonces/supprimeruneimage.php
@@ -5,7 +5,7 @@
 <form action="scripts/traiter_form.php" method="post">
 	<fieldset>
             <legend><h4><b>Supprimer une image:</b></h4></legend>
-            <br/> Merci de choisisir un utilisateur : <TEXTAREA name="nom" cols="20" rows="1" value=""></TEXTAREA>
+            <br/> Merci de choisir un utilisateur : <TEXTAREA name="nom" cols="20" rows="1" value=""></TEXTAREA>
             <br/> Merci de choisir une image : 
 </label>
 <INPUT type="submit" name="nom" value="Envoyer">
